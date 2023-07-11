66413b0ea3ae3d5438935fc45517bd5fefe9d1b8
Chris Toshok <toshok@gmail.com>
2023-07-11 14:06:04 -0700

    implement copyout command

--- cmd/copyout.go
+++ cmd/copyout.go
@@ -1,13 +1,106 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
+	"github.com/domainos-archeology/apollofs/fs"
 	"github.com/spf13/cobra"
 )
 
-func CopyOut(diskImage string, paths []string) error {
-	return fmt.Errorf("command not implemented")
+var errNotAFile = errors.New("not a file")
+
+func CopyOut(diskImage string, args []string) error {
+	src := args[0]
+	dest := args[1]
+
+	pvol, err := fs.Mount(diskImage)
+	if err != nil {
+		return err
+	}
+	defer pvol.Unmount()
+
+	lvol := pvol.LV
+
+	vm := fs.NewVTOCManager(lvol)
+	nm := fs.NewNamingManager(lvol, vm)
+
+	uid, err := nm.Resolve(src)
+	if err != nil {
+		return err
+	}
+
+	vtoce, err := vm.GetEntryForUID(uid)
+	if err != nil {
+		return err
+	}
+
+	if vtoce.IsDirectory() {
+		return errNotAFile
+	}
+
+	vtoce.Print()
+
+	fileBlockDAddrs, err := vm.GetFMBlocks(vtoce)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(fileBlockDAddrs)
+
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	remainingLength := int64(vtoce.Header.CurrentLength)
+
+	for i, daddr := range fileBlockDAddrs {
+		block, err := lvol.ReadBlock(daddr)
+		if err != nil {
+			return err
+		}
+
+		if block.Header.ObjectUID != vtoce.Header.ObjectUID {
+			panic("block.ObjectUID != vtoce.ObjectUID")
+		}
+
+		if block.Header.PageWithinObject != int32(i) {
+			panic(fmt.Sprintf("block.PageWithinObject %d != i %d", block.Header.PageWithinObject, i))
+		}
+
+		if remainingLength >= 1024 {
+			// we can write the full length of the block data
+			n, err := destFile.Write(block.Data[:])
+			if err != nil {
+				return err
+			}
+			if n != 1024 {
+				panic("short write")
+			}
+			remainingLength -= int64(n)
+		} else {
+			// we need to truncate the write to remainingLength.  we're at the
+			// end of the file.
+			n, err := destFile.Write(block.Data[:remainingLength])
+			if err != nil {
+				return err
+			}
+			if n != int(remainingLength) {
+				panic("short write")
+			}
+
+			// we're done.  make sure there are somehow not more blocks
+			if i != len(fileBlockDAddrs)-1 {
+				panic("more blocks after the last one written")
+			}
+			break
+		}
+	}
+
+	return nil
 }
 
 func init() {
@@ -15,10 +108,11 @@ func init() {
 }
 
 var copyOutCommand = &cobra.Command{
-	Use:   "copyout",
-	Short: "Copy a file from the disk image to the host",
-	Long:  ``,
+	Use:   "copyout [src] [dest]",
+	Short: "Copy a src file from the disk image to the host",
+	Long:  "",
+	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
+		return CopyOut(diskImage, args)
 	},
 }
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/exp v0.0.0-20230711153332-06a737ee72cb // indirect
+	golang.org/x/sys v0.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
